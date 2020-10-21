7fbbf2411c984c56904010e3839f32f3e64b43c0
Nate <hard5340@kettering.edu>
2020-10-21 09:46:53 -0700

    Add README and code refactoring.

--- 5ps.c
+++ 5ps.c
@@ -6,7 +6,7 @@
 /*
 Authors: Nathaniel Harder, Brendan Embrey
 Assignment Number: 1
-Date of Submission:
+Date of Submission: 10/21/20
 Name of this file: 5ps.c
 Description of the program:
 5ps.c takes arguments supplied from the command line and parses them before passing them to the parse function
--- stat.c
+++ stat.c
@@ -9,7 +9,7 @@
 /*
 Authors: Nathaniel Harder, Brendan Embrey
 Assignment Number: 1
-Date of Submission:
+Date of Submission: 10/21/20
 Name of this file: stat.c
 Description of the program: 
 stat.c takes information from 5ps.c and searches for data related to a given process
@@ -29,13 +29,13 @@ void format_time(char *time_string, char *utime, char *stime) {
     int l_utime = strtol(utime, NULL, 10);
     int l_stime = strtol(stime, NULL, 10);
     int ticks_per_second = sysconf(_SC_CLK_TCK);
-    int seconds = (l_utime + l_stime) / ticks_per_second;
+    int secs = (l_utime + l_stime) / ticks_per_second;
     /* Divide seconds by 60 to get minutes, minutes by 60 to get hours, and take seconds mod 60 to get the remaining number of seconds
     after hours and minutes have been counted. Then print the data to time_string. */
-    int minutes = seconds / 60;
-    int hours = minutes / 60;
-    seconds = seconds % 60;
-    sprintf(time_string, "%02d:%02d:%02d", hours, minutes, seconds);
+    int mins = secs / 60;
+    int hrs = mins / 60;
+    secs = secs % 60;
+    sprintf(time_string, "%02d:%02d:%02d", hrs, mins, secs);
 }
 
 /*
@@ -73,23 +73,17 @@ Brief description of the task: finds a given data point from the /proc/ director
 into a given result_string string. 
 */
 void read_data(char *result_string, DIR *dir, char *filename, char *field) {
-    
-    char info[1024] = "";
     struct dirent *d;
     FILE *search;
     // Use the readdir and strcmp function to check that the given file exists.
     if((d = readdir(dir)) != NULL) {
         if(strcmp(d->d_name, filename) != 0) {
-            // Use popen to search for the requested data with the unix cut command,
-            // then copy the data to rsult string.
+            /* Use popen to search for the requested data with the unix cut command,
+             then copy the data to rsult string. */
             char command[300] = "";
             sprintf(command,  "cut -d \" \" -f %s %s", field, filename);
             search = popen(command, "r");
-            if (fgets(info, sizeof(info), search)) {
-                strcpy(result_string, info);
-            } else {
-                result_string = "";
-            }
+            fgets(result_string, 2047, search);
             pclose(search);
         } 
     }
@@ -104,6 +98,7 @@ Output(Return value): None.
 Brief description of the task: decides to search for and prints values if they have been requested by command line arguments.
 */
 void search_data(DIR *dir, int state_selected, int time_selected, int memory_selected, int command_line_selected) {
+    /* Declare various strings for use later. */
     char state_string[1];
     char time_string[8]; 
     char mem_string[13];
@@ -160,11 +155,11 @@ void move_to_dir(char *pid, int state_selected, int time_selected, int memory_se
     dir = opendir(p_dir);
     if(dir == NULL) {
         // Exit program.
-        printf("Error: no process with selected PID.");
+        printf("Error: no process with selected PID.\n");
         exit(0);
     }
     chdir(p_dir); // Change current directory to process directory.
     printf("%s: ", pid);
-    
+    // Call search_data to begin finding data in /proc/.
     search_data(dir, state_selected, time_selected, memory_selected, command_line_selected);
 }
