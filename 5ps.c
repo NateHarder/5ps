903c3210165997fe768eb3c1c61832350887c097
Nate <hard5340@kettering.edu>
2020-10-21 14:41:48 -0700

    New comments and format.

--- 5ps.c
+++ 5ps.c
@@ -9,47 +9,99 @@ Assignment Number: 1
 Date of Submission: 10/21/20
 Name of this file: 5ps.c
 Description of the program:
-5ps.c takes arguments supplied from the command line and parses them before passing them to the parse function
-in stat.c file to print the disired data. 
+5ps.c takes arguments supplied from the command line and parses them before 
+passing them to the parse function in stat.c file to print the desired data. 
 */
 
 /*
+Function Name: check_arg
+Input to the method: argument string.
+Brief description of the task: checks to make sure supplied
+argument has no extra characters.
+*/
+void check_arg(char *argument) {
+    /*If an invalid option is selected, print an error and exit.*/
+    if(argument[0] == '-' && strlen(argument) > 2) {
+        argument++; // Move argument pointer to the invalid options in string.
+        printf("%s not recognized .\n", argument);
+        exit(0);
+    }
+}
+
+/*
+Function Name: check_pid_valid
+Input to the method: pid string.
+Brief description of the task: checks to make sure supplied
+pid is numerical.
+*/
+void check_pid_valid(char *pid) {
+    /* If no letters are selected, ensure that the argument is a valid pid. */
+    for(int pid_index = 0; pid_index < strlen(pid); pid_index++) {
+        if(!isdigit(pid[pid_index])) {
+            // Print an error and exit if invalid.
+            printf("Error: PID %s is invalid.\n", pid);
+            exit(0);
+        } 
+    } 
+}
+
+/*
+Function Name: handle_pid
+Input to the method: pid string and argument boolean values as integers.
+Brief description of the task: checks to make sure -p argument is use
+and pid is selected before continuing.
+*/
+void handle_pid(char *pid, int proc, int state, int time, int mem, int cmd) {
+    /* If no pid is selected as an argument, default to a pid of 1. */
+    if(proc == 0) {
+        pid[0] = '1';
+    } else {
+    /* If the argument -p is used and no pid is given, exit 
+    with an error message. */
+        if (strcmp(pid, "") == 0) {
+            printf("A PID must be selected with option -p.\n");
+            exit(0);
+        }
+    }
+    // Otherwise, continue to search for the data.
+    move_to_dir(pid, state, time, mem, cmd);
+}
+
+/*
 Function Name: handle args
-Input to the method: command line arguments for -p (PID), -s (state), -t (time), -c (command line), -v (memory used), and a pid number, as well as
+Input to the method: command line arguments for -p (PID), -s (state), 
+-t (time), -c (command line), -v (memory used), and a pid number, as well as
 boolean values stored as integers for the command line arguments.
 Output(Return value): None. 
-Brief description of the task: takes command line options and interprets data from them before passing them to be parsed.
+Brief description of the task: takes command line options and interprets data 
+from them before passing them to be parsed.
 */
-void handle_args(int argc, char *argv[], int process_information, int state_information, int time, int memory, int command_line, char pid[]) {
-    
-    /* loop through command line arguments and set a boolean to true if 
-    the corresponding letter is found. */
-    int pid_arg;
+void handle_args(int argc, char **argv, char *pid) {
+    // Initialize data.
+    int process = 0;
+    int state = 0;
+    int time = 0;
+    int memory = 0;
+    int command_line = 0;
+    /* loop through arguments and set bool to true if the letter is found. */
+    int pid_arg; // used to remember position of pid number in command line.
     for(int index = 1; index < argc; index++) {
-                if(strlen(argv[index]) > 1) {
-                    if (argv[index][2]) {
-                        printf("Error: option % is invalid.\n", argv[index]);
-                        exit(0);
-                    }
-                }
+                check_arg(argv[index]); // Check for extra characters.
                 switch (argv[index][1]) {
                     case 'p':
-                        process_information = 1;
-                        if (argv[index+1]) {
-                            /* If no letters are selected, ensure that the argument is a valid pid. */
-                            for(int pid_index = 0; pid_index < strlen(argv[index+1]); pid_index++) {
-                                if(!isdigit(argv[index+1][pid_index])) {
-                                    // Print an error and exit if invalid.
-                                    printf("Error: invalid PID.\n");
-                                    exit(0);
-                                } 
-                            }
+                        process = 1;
+                        if (argv[index+1]) { // Check if there is a PID.
+                            check_pid_valid(argv[index+1]);
+                            /* If the argument is a number, set pid to the
+                            number and use pid_arg to store pid position. */
                             pid = argv[index+1];
                             pid_arg = index+1;
                         }
                         break;
+                    /* If the argument is -s, -t, -c, or -v, set the boolean 
+                    value for that argument to 1.*/    
                     case 's':
-                        state_information = 1;
+                        state = 1;
                         break;
                     case 't':
                         time = 1;
@@ -61,54 +113,30 @@ void handle_args(int argc, char *argv[], int process_information, int state_info
                         memory = 1;
                         break;
                     default:
-                    
-                        //for(int pid_index = 0; pid_index < strlen(argv[index]); pid_index++) {
-                          //  if(!isdigit(argv[index][pid_index])) {
-                                // Print an error and exit if invalid.
-                            //    printf("Error: invalid input.\n");
-                              //  exit(0);
-                            //} 
-                        //}
-                        // If the argument is a number, set the value of pid to the arguement.
-                        
+                        /* If the index is not the same as the stored pid's,
+                        print an error because there is an invalid argument. */
                         if (index != pid_arg) {
-                            printf("Error: option not recognized.\n");
+                            printf("Error: %s not recognized.\n", argv[index]);
                             exit(0);
                         }
-                    break;
+                        break; // Argument is a pid, so skip to the next one.
                 }
     }
-    /* If no pid is selected as an argument, default to a pid of 1. */
-
-    /* If the argument -p is used and a pid is given, call parse to return the requested data. */
-    if(process_information == 0) {
-        pid[0] = '1';
-    } else {
-        if (strcmp(pid, "") == 0) {
-            printf("A PID must be selected with option -p.\n");
-            exit(0);
-        }
-    }
-    move_to_dir(pid, state_information, time, memory, command_line);
+    handle_pid(pid, process, state, time, memory, command_line);
 }
 
 /*
 Function Name: main
-Input to the method: command line arguments for -p (PID), -s (state), -t (time), -c (command line), -v (memory used), and a pid number.
+Input to the method: command line arguments for -p (PID), -s (state), 
+-t (time), -c (command line), -v (memory used).
 Output(Return value): returns 0 if program executes successfully. 
-Brief description of the task: sets boolean values to zero, initializes pid to an empty string, and passes them all to handle args.
+Brief description of the task: initializes pid to an empty string, and passes 
+pid and arguments to handle_args.
 */
-int main(int argc, char *argv[]) {
-
-// Initialize data.
-int process_information = 0;
-int state_information = 0;
-int time = 0;
-int memory = 0;
-int command_line = 0;
-char pid[9] = "";
-
+int main(int argc, char **argv) {
+// Declare pid to be 9 characters long.
+char pid[9];
 // Begin handling of arguments.
-handle_args(argc, argv, process_information, state_information, time, memory, command_line, pid);
+handle_args(argc, argv, pid);
 return 0;
 }
--- stat.c
+++ stat.c
@@ -12,26 +12,28 @@ Assignment Number: 1
 Date of Submission: 10/21/20
 Name of this file: stat.c
 Description of the program: 
-stat.c takes information from 5ps.c and searches for data related to a given process
-before printing that information to stdout.
+stat.c takes information from 5ps.c and searches for data related to a given 
+process before printing that information to stdout.
 */
 
 /*
 Function Name: format_time
-Input to the method: A string for printing time in HH:MM:SS format as well as two string representing 
-the utime and stime values of a process.
+Input to the method: A string for printing time in HH:MM:SS format as well as 
+two string representing the utime and stime values of a process.
 Output(Return value): None.
-Brief description of the task: receives string values for utime and stime values in stat file,
-then prints them to time_string string in format HH:MM:SS.
+Brief description of the task: receives string values for utime and stime 
+values in stat file, then prints them to time_string string in format HH:MM:SS.
 */
 void format_time(char *time_string, char *utime, char *stime) {
-    // Convert utime and stime to integers, then divide by ticks per second to get total seconds.
+    /* Convert utime and stime to integers, then divide by ticks per second to
+    get total seconds. */
     int l_utime = strtol(utime, NULL, 10);
     int l_stime = strtol(stime, NULL, 10);
     int ticks_per_second = sysconf(_SC_CLK_TCK);
     int secs = (l_utime + l_stime) / ticks_per_second;
-    /* Divide seconds by 60 to get minutes, minutes by 60 to get hours, and take seconds mod 60 to get the remaining number of seconds
-    after hours and minutes have been counted. Then print the data to time_string. */
+    /* Divide seconds by 60 to get minutes, minutes by 60 to get hours, and 
+    take seconds mod 60 to get the remaining number of seconds after hours and 
+    minutes have been counted. Then print the data to time_string. */
     int mins = secs / 60;
     int hrs = mins / 60;
     secs = secs % 60;
@@ -42,9 +44,10 @@ void format_time(char *time_string, char *utime, char *stime) {
 Function Name: cut_newline
 Input to the method: A string that may have a newline character at the end.
 Output(Return value): None.
-Brief description of the task: removes a newline character from a string if present.
-*/
+Brief description of the task: removes a newline character from a string if 
+present. */
 void cut_newline(char *data_string) {
+    /* If final character is newline, replace it with a null character. */
     int last = strlen(data_string) - 1;
     if(data_string[last] == '\n') {
         data_string[last] = '\0';
@@ -67,10 +70,12 @@ void replace_paren(char *data_string) {
 
 /*
 Function Name: read_data
-Input to the method: A string to print data to, DIR and dirent variables for navigating a directory, and a filename and field number stored as strings.
+Input to the method: A string to print data to, DIR and dirent variables for 
+navigating a directory, and a filename and field number stored as strings.
 Output(Return value): None.
-Brief description of the task: finds a given data point from the /proc/ directory, removes any newlines, and copies the data
-into a given result_string string. 
+Brief description of the task: finds a given data point from the /proc/ 
+directory, removes any newlines, and copies the data into a given 
+result_string string. 
 */
 void read_data(char *result_string, DIR *dir, char *filename, char *field) {
     struct dirent *d;
@@ -78,11 +83,12 @@ void read_data(char *result_string, DIR *dir, char *filename, char *field) {
     // Use the readdir and strcmp function to check that the given file exists.
     if((d = readdir(dir)) != NULL) {
         if(strcmp(d->d_name, filename) != 0) {
-            /* Use popen to search for the requested data with the unix cut command,
-             then copy the data to rsult string. */
+            /* Use popen to search for the requested data with the unix cut 
+            command, then copy the data to result_string. */
             char command[300] = "";
             sprintf(command,  "cut -d \" \" -f %s %s", field, filename);
             search = popen(command, "r");
+            /* Retrieve up to 2047 chars, thw max for linux command line */
             fgets(result_string, 2047, search);
             pclose(search);
         } 
@@ -92,45 +98,52 @@ void read_data(char *result_string, DIR *dir, char *filename, char *field) {
 
 /*
 Function Name: search_data
-Input to the method: DIR and dirent variables for navigating a directory as well as boolean values stored as integers
-for handling command line arguments.
+Input to the method: DIR and dirent variables for navigating a directory as 
+well as boolean values stored as integers for handling command line arguments.
 Output(Return value): None.
-Brief description of the task: decides to search for and prints values if they have been requested by command line arguments.
+Brief description of the task: decides to search for and prints values if they
+have been requested by command line arguments.
 */
-void search_data(DIR *dir, int state_selected, int time_selected, int memory_selected, int command_line_selected) {
+void search_data(DIR *dir, int state, int time, int memory, int cmdline) {
     /* Declare various strings for use later. */
     char state_string[1];
-    char time_string[8]; 
+    char time_string[8]; // Arrays are declared as their max possible length.
     char mem_string[13];
-    char cmd_string[1024] = "";
+    char cmd_string[2047] = ""; 
     char utime[8];
     char stime[8];
-    /* If -s is selected as an argument, find the state information in field 3 of the stat file and print. */
-    if(state_selected == 1) {
+    /* If -s is selected as an argument, find the state information in field 3
+    of the stat file and print. */
+    if(state == 1) {
         read_data(state_string, dir, "stat", "3");
         printf("%s ", state_string);
     }
-    /* If -t is selected as an argument, find the utime and stime information in fields 14 and 15 of the stat file,
-    then format them in HH:MM:SS format and print. */
-    if(time_selected == 1) {
+    /* If -t is selected as an argument, find the utime and stime information 
+    in fields 14 and 15 of the stat file, then format them in HH:MM:SS format 
+    and print. */
+    if(time == 1) {
         read_data(utime, dir, "stat", "14");
         read_data(stime, dir, "stat", "15");
         format_time(time_string, utime, stime);
         printf("time=%s ", time_string);
     }
-    /* If -v is selected as an argument, find the memory information in line 1 of the statm file and print it. */  
-    if(memory_selected == 1) {
+    /* If -v is selected as an argument, find the memory information in line 1
+    of the statm file and print it. */  
+    if(memory == 1) {
         read_data(mem_string, dir, "statm", "1");
         printf("sz=%s ", mem_string);
     }
-    /* If -c is selected as an argument, find the command information in line 1 of the cmdline file and print it. */
-    if(command_line_selected == 1) {
+    /* If -c is selected as an argument, find the command information in line 
+    1 of the cmdline file and print it. */
+    if(cmdline == 1) {
         read_data(cmd_string, dir, "cmdline", "1");
-        // Check if cmdline file returns empty, and read command from the second field of stat file if so.
+        /* Check if cmdline file returns empty, and read command from the 
+        second field of stat file if so. */
         if (cmd_string[0] == '\0') {
             read_data(cmd_string, dir, "stat", "2");
             replace_paren(cmd_string);
         } else {
+            /* Otherwise just print the command */
             printf("[%s]", cmd_string);
         }
     }
@@ -140,26 +153,27 @@ void search_data(DIR *dir, int state_selected, int time_selected, int memory_sel
     
 /*
 Function Named move_to_dir
-Input to the method: PID value stored in string as well as boolean dat stored as integer for command line options.
+Input to the method: PID value stored in string as well as boolean dat stored 
+as integer for command line options.
 Output(Return value): None.
-Brief description of the task: takes given data from the calling function and finds data if it has been selected to be returned
-with integer arguments used as booleans.
+Brief description of the task: takes given data from the calling function and 
+finds data if it has been selected to be returned with integer arguments used 
+as booleans.
 */
-void move_to_dir(char *pid, int state_selected, int time_selected, int memory_selected, int command_line_selected) {
+void move_to_dir(char *pid, int state, int time, int memory, int cmdline) {
     DIR *dir;
-    /* Declare various strings used to handle the data points that are being requested. */
-
     char p_dir[255];
     sprintf(p_dir, "/proc/%s", pid);
-    /* Open the /proc/[pid] directory based on the pid number chosen. Exit if no process found. */
+    /* Open the /proc/[pid] directory based on the pid number chosen. Exit if 
+    no process found. */
     dir = opendir(p_dir);
     if(dir == NULL) {
-        // Exit program.
+        // Exit program if pis is not found.
         printf("Error: no process with selected PID.\n");
         exit(0);
     }
     chdir(p_dir); // Change current directory to process directory.
     printf("%s: ", pid);
     // Call search_data to begin finding data in /proc/.
-    search_data(dir, state_selected, time_selected, memory_selected, command_line_selected);
+    search_data(dir, state, time, memory, cmdline);
 }
